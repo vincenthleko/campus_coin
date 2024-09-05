aecec601c5a015be86f14a4a5da2568dea986db5
Vincent~ <vincenthleko95@gmail.com>
2024-09-05 14:33:28 +0200

    added dashboard page that takes you to reedemPoint page

--- public/alpineCampusCoin.js
+++ public/alpineCampusCoin.js
@@ -5,13 +5,16 @@ document.addEventListener("alpine:init", () => {
         selectedPlatform: "",
         
         async fetchPoints(platform) {
+            console.log(`Fetching points for ${platform} and username ${this.username}`); // Debugging statement
             this.selectedPlatform = platform;
             const response = await fetch(`/api/${platform}/username/${this.username}`);
             const data = await response.json();
+            console.log(data); // Add this line to debug the API response
             this.points = data.points;
         },
 
         async fetchOverallPoints() {
+            console.log(`Fetching overall points for username ${this.username}`); // Debugging statement
             this.selectedPlatform = 'overall';
             const platforms = ['checkers60', 'takealot', 'uber', 'uberEats'];
             let totalPoints = 0;
@@ -19,6 +22,7 @@ document.addEventListener("alpine:init", () => {
             for (let platform of platforms) {
                 const response = await fetch(`/api/${platform}/username/${this.username}`);
                 const data = await response.json();
+                console.log(`Points from ${platform}: ${data.points}`); // Debugging statement
                 totalPoints += data.points;
             }
 
@@ -54,6 +58,10 @@ document.addEventListener("alpine:init", () => {
             } else {
                 this.errorMessage = data.message;
             }
+        },
+
+        goBackToDashboard() {
+            window.location.href = 'dashboard.html';  // Use relative path to redirect to dashboard.html
         }
     }));
 });
