68464b2242ded3d2552fc68d74cd3643130a2e2c
Loyiso-Xaba <xloyiso@gmail.com>
2024-10-18 09:34:03 +0200

    request and lending page

--- public/nsfasWeb3.js
+++
@@ -1,8 +0,0 @@
-const Web3 = require("web3");
-const INFURA_ID = '3c63b0011e354e4f8f996debd85fbb2f'
-async function connect() {
-    const provider = new Web3.providers.JsonRpcProvider(`https://linea-sepolia.infura.io/v3/${INFURA_ID}`);
-    const web3 = new Web3(provider);
-    console.log(web3);
-}
-connect();
